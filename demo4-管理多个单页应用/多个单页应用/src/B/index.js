a4dfb0111f594d3ffa0ef2019ccbaf2cb7215d43
h23 <qzhuang92@126.com>
2018-08-21 17:27:52 +0800

    删除多余文件

--- demo4-管理多个单页应用/多个单页应用/src/B/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-class B extends Component {
-    render() {
-        return <h1>Hello word! I'm B.</h1>
-    }
-}
-ReactDOM.render(<B />, document.getElementById('app'));
--- demo4-管理多个单页应用/多入口/src/A.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-class A extends Component {
-    render() {
-        return <h1>Hello word! I'm A.</h1>
-    }
-}
-ReactDOM.render(<A />, document.getElementById('A'));
